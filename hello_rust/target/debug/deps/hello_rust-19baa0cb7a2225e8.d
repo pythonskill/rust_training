363f71c784633cf345fbf4df6a8ec9894c7c8a09
Art <pythonskill@yandex.ru>
2026-01-01 15:11:31 +0000

    git init env

--- hello_rust/target/debug/deps/hello_rust-19baa0cb7a2225e8.d
+++ hello_rust/target/debug/deps/hello_rust-19baa0cb7a2225e8.d
@@ -5,5 +5,5 @@
 src/lib.rs:
 Cargo.toml:
 
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_ARGS=
 # env-dep:CLIPPY_CONF_DIR
---
+++ hello_rust/target/debug/deps/hello_rust-a38363a66d5aa94e.d
@@ -0,0 +1,9 @@
+/workspaces/rust_training/hello_rust/target/debug/deps/hello_rust-a38363a66d5aa94e.d: src/lib.rs Cargo.toml
+
+/workspaces/rust_training/hello_rust/target/debug/deps/libhello_rust-a38363a66d5aa94e.rmeta: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
